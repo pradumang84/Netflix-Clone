0b586496094404640a792ca2213eb444c494b94a
Praduman Gupta <Pradumangup84@gmail.com>
2026-09-20 18:04:57 +0530

    Fix production API URL

--- Netflix/src/utils/constant.js
+++ Netflix/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "http://localhost:3000/api/v1/user";
+export const API_END_POINT = "https://netflix-kw38.onrender.com/api/v1/user";
 
 export const options = {
   method: "GET",
